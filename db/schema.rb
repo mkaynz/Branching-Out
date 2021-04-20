c1ccffb4fec044ea6543a77d118b12361ec50f89
Cayla Taylor <ctylor50@memphis.edu>
2021-04-20 17:10:48 -0500

    US4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_004236) do
+ActiveRecord::Schema.define(version: 2021_04_16_005701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2021_04_13_004236) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,4 +64,5 @@ ActiveRecord::Schema.define(version: 2021_04_13_004236) do
 
   add_foreign_key "ads", "plants"
   add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "users"
 end
